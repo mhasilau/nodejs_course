5f8b42a1f6fc9c504ecc29b90e3e43488ef1da02
mhasilau <mikhail.hasilau@gmail.com>
2024-10-04 11:53:54 +0300

    fix validation

--- home-3097-form/home-3097.js
+++ home-3097-form/home-3097.js
@@ -3,40 +3,43 @@ const express = require('express');
 const webServer = express()
 const port = 7480;
 
-const form ='    <div>\n' +
-    '        <form action="http://178.172.195.18:7480/mood" method="get">\n' +
-    '            <div>\n' +
-    '                <label for="name">Представьтесь:</label>\n' +
-    '                <input type="text" id="name" name="name">\n' +
-    '            </div>\n' +
-    '            <div>\n' +
-    '                <label for="mood">Настроение:</label>\n' +
-    '                <select id="mood" name="mood" required>\n' +
-    '                    <option value="excellent">Отличное</option>\n' +
-    '                    <option value="good">Хорошее</option>\n' +
-    '                    <option value="nothing">Никакое</option>\n' +
-    '                    <option value="bad">Плохое</option>\n' +
-    '                    <option value="disgusting">Отвратительное</option>\n' +
-    '                </select>\n' +
-    '            </div>\n' +
-    '            <input type="submit" value="Отправить">\n' +
-    '        </form>\n' +
-    '    </div>'
+function createForm(name = '') {
+    return `
+    <div>
+        <form action="/mood" method="get">
+            <div>
+                <label for="name">Представьтесь:</label>
+                <input type="text" id="name" name="name" value="${name}">
+            </div>
+            <div>
+                <label for="mood">Настроение:</label>
+                <select id="mood" name="mood" required>
+                    <option value="excellent">Отличное</option>
+                    <option value="good">Хорошее</option>
+                    <option value="nothing">Никакое</option>
+                    <option value="bad">Плохое</option>
+                    <option value="disgusting">Отвратительное</option>
+                </select>
+            </div>
+            <input type="submit" value="Отправить">
+        </form>
+    </div>`;
+}
 
 const errorMessageShortName = '    <div>\n' +
     '        <span style="color: red">Тебя не могу звать одной буквой...</span>\n' +
-    '    </div>'
+    '    </div>';
 const errorMessageEmptyName = '    <div>\n' +
     '        <span style="color: red">Похоже ты забыл представиться...</span>\n' +
-    '    </div>'
+    '    </div>';
 
 webServer.get('/', (req, res) => {
-    res.send(form)
-})
+    res.send(createForm());
+});
 
 webServer.get('/mood', (req, res) => {
-    const userName = req.query.name.toString()
-    const mood = req.query.mood
+    const userName = req.query.name ? req.query.name.toString().trim() : '';
+    const mood = req.query.mood;
     let message;
 
     switch (mood) {
@@ -44,32 +47,32 @@ webServer.get('/mood', (req, res) => {
             message = 'Я рад, что у тебя все настолько здорово!';
             break;
         case 'good':
-            message = 'Хорошее настроение - это хорошо'
+            message = 'Хорошее настроение - это хорошо';
             break;
         case 'nothing':
-            message ='Штош... Не вешай нос, это, во всяком случае, не плохо'
+            message = 'Штош... Не вешай нос, это, во всяком случае, не плохо';
             break;
         case 'bad':
-            message ='У тебя все наладится!'
+            message = 'У тебя все наладится!';
             break;
         case 'disgusting':
-            message ='Я за плинтусом. Отпустит - позови.'
+            message = 'Я за плинтусом. Отпустит - позови.';
             break;
     }
 
-    if (!userName.trim().length) {
-        return res.send(form + errorMessageEmptyName)
+    if (!userName.length) {
+        return res.send(createForm(userName) + errorMessageEmptyName);
     }
 
-    if (userName.trim().length < 2) {
-        return res.send(form + errorMessageShortName)
+    if (userName.length < 2) {
+        return res.send(createForm(userName) + errorMessageShortName);
     }
 
-    res.send(`Привет ${userName}. ${message}`)
+    res.send(`Привет ${userName}. ${message}`);
 
-    console.log(`Привет ${userName}. ${message}`)
-})
+    console.log(`Привет ${userName}. ${message}`);
+});
 
 webServer.listen(port, () => {
     console.log(`Server started on port ${port}`);
-})
+});
